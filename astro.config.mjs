c71e488dc82097c67449455e0c52f73ab3bbbb55
BrainB0ne <1330707+BrainB0ne@users.noreply.github.com>
2025-12-28 21:32:17 +0100

    added astro-favicons package

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import pagefind from "astro-pagefind";
 import tailwindcss from "@tailwindcss/vite";
+import favicons from "astro-favicons";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.brainbytez.eu",
-  integrations: [sitemap(), mdx(), pagefind()],
+  integrations: [sitemap(), mdx(), pagefind(), favicons()],
   vite: {
     plugins: [tailwindcss()],
   },
