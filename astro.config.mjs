4d5c99309debacc4f87ea922e8f95cfd1663308e
BrainB0ne <1330707+BrainB0ne@users.noreply.github.com>
2026-01-02 03:32:42 +0100

    Revert "added manual setup of @astrojs/cloudflare package"
    This reverts commit f3622b3697fd43193dcd4139c1bbadb2f02c636a.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,13 @@ import mdx from "@astrojs/mdx";
 import pagefind from "astro-pagefind";
 import tailwindcss from "@tailwindcss/vite";
 import favicons from "astro-favicons";
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.brainbytez.eu",
-  adapter: cloudflare({
-     imageService: 'compile'
-  }),
   integrations: [sitemap(), mdx(), pagefind(), favicons()],
   vite: {
     plugins: [tailwindcss()],
-    ssr: {
-      external: ['node:path/posix'],
-    },
   },
   markdown: {
     shikiConfig: {
