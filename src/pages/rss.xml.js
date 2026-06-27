de9f51845855f4d7692bb0f4cce7954f9e72a041
BrainB0ne <1330707+BrainB0ne@users.noreply.github.com>
2026-06-27 17:38:34 +0200

    added simplified RSS previews
    Now each feed item’s content is:
    
    <p>{frontmatter description}</p>
    <p><a href="{full article URL}">Continue reading</a></p>
    
    Preview length is controlled by the frontmatter description.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,6 +3,15 @@ import { SITE } from "@/consts";
 import { getCollection } from "astro:content";
 import { getEntryDate, sortByDateDesc } from "@/lib/utils";
 
+function escapeHtml(value) {
+  return value
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#39;");
+}
+
 export async function GET(context) {
   const blog = (await getCollection("blog")).filter((post) => !post.data.draft);
 
@@ -16,11 +25,18 @@ export async function GET(context) {
     title: SITE.TITLE,
     description: SITE.DESCRIPTION,
     site: context.site,
-    items: items.map((item) => ({
-      title: item.data.title,
-      description: item.data.description,
-      pubDate: getEntryDate(item),
-      link: `/${item.collection}/${item.id}/`,
-    })),
+    items: items.map((item) => {
+      const link = `/${item.collection}/${item.id}/`;
+      const fullUrl = new URL(link, context.site).toString();
+      const preview = escapeHtml(item.data.description);
+
+      return {
+        title: item.data.title,
+        description: item.data.description,
+        pubDate: getEntryDate(item),
+        link,
+        content: `<p>${preview}</p><p><a href="${fullUrl}">Continue reading</a></p>`,
+      };
+    }),
   });
 }
